43e580c4fff9ed2676cd4520f9fb733d9b7d7bae
JLopez80 <jetlilopez731@gmail.com>
2025-11-06 11:06:08 -0600

    Testing

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
+# main.py
+from file_carver import recover_files
+from monitor import start_monitoring
+import os
 
+MONITORED_FOLDER = input("Enter folder to monitor: ").strip()
+SNAPSHOT_FOLDER = os.path.join(MONITORED_FOLDER, ".snapshot")
+RECOVERY_FOLDER = os.path.join(os.getcwd(), "Recovered")
+
+os.makedirs(SNAPSHOT_FOLDER, exist_ok=True)
+os.makedirs(RECOVERY_FOLDER, exist_ok=True)
+
+# Take initial snapshot of the folder
+recover_files(MONITORED_FOLDER, SNAPSHOT_FOLDER)
+
+print(f"[*] Starting File Recovery Tool on {MONITORED_FOLDER}...")
+start_monitoring(MONITORED_FOLDER, SNAPSHOT_FOLDER, RECOVERY_FOLDER)
